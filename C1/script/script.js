e61dd737a620d2e6682c7d53b1523cbd3fcc0243
ParkGangSa <123992020+ParkGangSa@users.noreply.github.com>
2023-10-26 09:31:59 +0900

    Add files via upload

---
+++ C1/script/script.js
@@ -0,0 +1,42 @@
+$(function(){
+  // Menu
+  $(".nav > ul > li").mouseover(function(){
+    $(this).find(".submenu").stop().slideDown();
+  });
+  $(".nav > ul > li").mouseout(function(){
+    $(this).find(".submenu").stop().slideUp();
+  });
+
+  // 이미지 슬라이드
+  let currentIndex = 0;
+
+  setInterval(function(){
+    let nextIndex = (currentIndex + 1) % 3;
+    $(".slider").eq(currentIndex).fadeOut(1200);
+    $(".slider").eq(nextIndex).fadeIn(1200);
+
+    currentIndex = nextIndex;
+  }, 3000);
+
+  // 탭메뉴
+  const tabBtn = $(".info-menu > a");
+  const tabCont = $(".info-cont > div");
+  tabCont.hide().eq(0).show();
+
+  tabBtn.on("click", function(){
+    const index = $(this).index();
+
+    $(this).addClass("active").siblings().removeClass("active");
+    tabCont.eq(index).show().siblings().hide();
+  });
+
+  // popup
+  $(".popup-btn").click(function(){
+    $(".popup-view").show();
+    $(".popup-bg").show();
+  });
+  $(".popup-close").click(function(){
+    $(".popup-view").hide();
+    $(".popup-bg").hide();
+  });
+});
