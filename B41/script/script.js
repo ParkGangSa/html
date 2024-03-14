7849dda858dd15169eca303fffd9601e23050623
ParkGangSa <123992020+ParkGangSa@users.noreply.github.com>
2024-03-14 09:45:46 +0900

    Add files via upload

---
+++ B41/script/script.js
@@ -0,0 +1,34 @@
+$(function(){
+  // 메뉴
+  $("nav > ul > li").mouseover(function(){
+    $(".sub, .menubg").stop().slideDown();
+  });
+  $("nav > ul > li").mouseout(function(){
+    $(".sub, .menubg").stop().slideUp();
+  });
+
+  // 팝업
+  $(".pp").click(function(){
+    $(".popup").show();
+    return false;
+  });
+  $(".popup .close").click(function(){
+    $(".popup").hide();
+    return false;
+  });
+
+  // 슬라이드
+  let i = 0;
+  $(".slider ul").append($(".slider ul li").first().clone(true));
+
+  setInterval(function(){
+    i++;
+    $(".slider ul").animate({marginLeft: i * -1200 + 'px'}, 600);
+    if(i == 3) {
+      setTimeout(function(){
+        $(".slider ul").animate({marginLeft: 0}, 0);
+        i = 0;
+      },700);
+    }
+  }, 3000);
+});
